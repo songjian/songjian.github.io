e75cceb1ef3ad3ffc2bfa75b0d4a53fe7ad5bfee
宋健 <songj@maxucode.top>
2021-06-17 13:43:25 +0800

    添加docker-compose配置和修改发布脚本

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone git@github.com:songjian/songjian.github.io.git /tmp/songjian.github.io.git
-jekyll build -d /tmp/songjian.github.io.git
+docker-compose run app /bin/bash -c "gem sources --add https://mirrors.tuna.tsinghua.edu.cn/rubygems/ --remove https://rubygems.org/ && gem install bundler:2.1.4 && bundle _2.1.4_ i && jekyll b"
+rsync -av _site/ /tmp/songjian.github.io.git
 cd /tmp/songjian.github.io.git
 git add .
 git commit -m Publish
