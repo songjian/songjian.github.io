049189f1ebafa9a5838a791faaa00a5c8a793933
宋健 <songjian@codeorder.cn>
2021-07-28 06:46:17 +0800

    fix bug

--- publish-to-github.io.sh
+++ publish-to-github.io.sh
@@ -3,7 +3,7 @@ docker-compose down
 docker-compose run --rm app jekyll b
 git clone git@github.com:songjian/songjian.github.io.git /tmp/songjian.github.io
 rsync -av _site/ /tmp/songjian.github.io
-cd /tmp/songjian.github.io.git
+cd /tmp/songjian.github.io
 git add .
 git commit -m Publish
 git push
