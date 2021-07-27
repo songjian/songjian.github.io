b67fbe368c96f9572694be759037e92f74308ceb
宋健 <songjian@codeorder.cn>
2021-07-28 06:08:39 +0800

    Publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-git clone git@github.com:songjian/songjian.github.io.git /tmp/songjian.github.io.git
-docker-compose run app /bin/bash -c "gem sources --add https://mirrors.tuna.tsinghua.edu.cn/rubygems/ --remove https://rubygems.org/ && gem install bundler:2.1.4 && bundle _2.1.4_ i && jekyll b"
-rsync -av _site/ /tmp/songjian.github.io.git
+docker-compose down
+docker-compose run --rm app jekyll b
+git clone git@github.com:songjian/songjian.github.io.git /tmp/songjian.github.io
+rsync -av _site/ /tmp/songjian.github.io
 cd /tmp/songjian.github.io.git
 git add .
 git commit -m Publish
